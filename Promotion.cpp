12355f31d7ae83959d0b249f100e22b5659a4de2
koko baghdad <kokobaghdad123@gmail.com>
2022-05-26 15:13:18 +0200

    Solving Infinite Loop in checkAutoPromotion.

--- Promotion.cpp
+++ Promotion.cpp
@@ -8,7 +8,7 @@ Promotion::Promotion(Time eventTime, int ID, int extra) :Event(eventTime, ID)
 void Promotion::Execute(Linked_list<Cargo>& cn, LinkedQueue<Cargo>& cs, PriorityQueue<Cargo>& cv)
 {
 	Cargo Item;
-	cn.removeID(Item.getID(), Item);								 //move it from normal list to vip list
+	cn.removeID(ID, Item);								 //move it from normal list to vip list
 	Item.setCargoTyp('V');											 //changing its typ from normal to vip
 	Item.setCost(Item.getCost() + extraMoney);
 	cv.enqueue(Item, Item.getCost() + Item.getDeliveryDistance());
